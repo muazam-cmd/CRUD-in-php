c6a4f601394366113b887460b94f4cf10be85dc5
Muazam Mzm <63458163+muazam-cmd@users.noreply.github.com>
2022-02-12 17:38:08 +0500

    Add files via upload

---
+++ delete-inline.php
@@ -0,0 +1,10 @@
+<?php 
+echo $stu_id = $_GET['id'];
+include 'config.php';
+$sql = "DELETE FROM student WHERE sid={$stu_id}";
+$result = mysqli_query($conn, $sql) or die("query unsucessful!");
+
+header("Location: http://localhost/crud/index.php");
+mysqli_close($conn);
+
+?>
